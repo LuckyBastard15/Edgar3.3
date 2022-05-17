99b1f7da3962860094eb5507699e8a39feb60c9b
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2022-05-17 13:50:52 -0500

    Initial commit

---
+++ Assets/PenCanva.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PenCanva : MonoBehaviour, IPointerClickHandler
+{
+    public Action OnPenCanvasLeftClickEvent = default;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.pointerId == -1)
+        {
+            OnPenCanvasLeftClickEvent?.Invoke();
+        }
+    }
+}
